9b1fa3264aaee15450310221ce77a83c768a9cd9
ik-code <igo.metacon@gmail.com>
2021-04-27 12:33:05 +0300

    creating post and category models and migrations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,26 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-md-4">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href="/posts">Posts</a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="/categories">Categories</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        You're logged in!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
