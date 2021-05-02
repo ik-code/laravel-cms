e7023cfe5180825289086dfabe1934a09bb7dafe
ik-code <igo.metacon@gmail.com>
2021-05-02 21:49:15 +0300

    install frontend theme posttableseeder

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category1 = Category::create([
+           'name' => 'News'
+        ]);
+        $category2 = Category::create([
+            'name' => 'Marketing'
+        ]);
+        $category3 = Category::create([
+            'name' => 'Partnership'
+        ]);
+        $post1 = Post::create([
+           'title' => 'Man must explore, and this is exploration at its greatest',
+            'description' => 'Problems look mighty small from 150 miles up',
+            'post_content' => '<div><h2>Where does it come from?</h2><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p></div>',
+            'category_id' => $category1->id,
+            'image' => 'posts/1.jpg'
+        ]);
+        $post2 = Post::create([
+            'title' => 'I believe every human has a finite number of heartbeats. I don\'t intend to waste any of mine.',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus condimentum porta magna, eu eleifend purus congue a. ',
+            'post_content' => '<div><h2>Where does it come from?</h2><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p></div>',
+            'category_id' => $category2->id,
+            'image' => 'posts/2.jpg'
+        ]);
+        $post3 = Post::create([
+            'title' => 'Science has not yet mastered prophecy',
+            'description' => 'We predict too much for the next year and yet far too little for the next ten.',
+            'post_content' => '<div><h2>Where does it come from?</h2><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p></div>',
+            'category_id' => $category3->id,
+            'image' => 'posts/3.jpg'
+        ]);
+        $post4 = Post::create([
+            'title' => 'Failure is not an option',
+            'description' => 'Many say exploration is part of our destiny, but it’s actually our duty to future generations.',
+            'post_content' => '<div><h2>Where does it come from?</h2><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p></div>',
+            'category_id' => $category1->id,
+            'image' => 'posts/4.jpg'
+        ]);
+        $post5 = Post::create([
+            'title' => 'Maecenas vel dui a risus ornare congue sed a libero. Cras et lorem enim. ',
+            'description' => 'Raesent vel leo tempus enim ullamcorper porttitor ac vitae dolor. Sed quis eros sapien. Morbi et sem id nisi dignissim malesuada.',
+            'post_content' => '<div><h2>Where does it come from?</h2><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p></div>',
+            'category_id' => $category2->id,
+            'image' => 'posts/5.jpg'
+        ]);
+        $post6 = Post::create([
+            'title' => 'Aenean egestas tortor in tellus vestibulum facilisis.',
+            'description' => 'Nullam posuere ultrices odio id posuere. Suspendisse eget bibendum purus. Pellentesque pellentesque efficitur quam eget tristique.',
+            'post_content' => '<div><h2>Where does it come from?</h2><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p></div>',
+            'category_id' => $category3->id,
+            'image' => 'posts/6.jpg'
+        ]);
+
+        $tag1 = Tag::create([
+            'name' => 'job'
+        ]);
+        $tag2 = Tag::create([
+            'name' => 'customers'
+        ]);
+        $tag3= Tag::create([
+            'name' => 'record'
+        ]);
+        $post1->tags()->attach([$tag1->id, $tag2->id]);
+        $post2->tags()->attach([$tag2->id, $tag3->id]);
+        $post3->tags()->attach([$tag1->id, $tag3->id]);
+        $post4->tags()->attach([$tag1->id, $tag2->id]);
+        $post5->tags()->attach([$tag2->id, $tag3->id]);
+        $post6->tags()->attach([$tag1->id, $tag3->id]);
+    }
+}
